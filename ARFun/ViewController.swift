bf3708cfe2773c894d16e65b7bb67e35df31d154
Ricardo Hui <eehaha@gmail.com>
2019-04-19 08:34:45 +0800

    ARKit with movement

--- ARFun/ViewController.swift
+++ ARFun/ViewController.swift
@@ -28,6 +28,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        for node in sceneView.scene.rootNode.childNodes {
+            let moveShip = SCNAction.moveBy(x: 0, y: 0.5, z: 0.5, duration: 1)
+            let fadeOut = SCNAction.fadeOpacity(to: 0.5, duration: 1)
+            let fadeIn = SCNAction.fadeOpacity(to: 1.0, duration: 1)
+            let routine = SCNAction.sequence([fadeOut,fadeIn,moveShip])
+            let foreverRoutine = SCNAction.repeatForever(routine)
+            
+            node.runAction(foreverRoutine)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,36 +55,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
